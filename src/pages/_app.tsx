35663a5cf3f8ba5fa2f920c4eb7e3f5010641521
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2023-04-02 21:49:25 +0200

    chore: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import '../init/dayjs'
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <Analytics />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
