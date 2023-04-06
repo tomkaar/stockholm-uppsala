b4f63bdfcaa35762c69b1665ff8ba310204276c6
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2023-04-06 13:02:08 +0200

    style: update page error message

--- app/[from]/[to]/[day]/error.tsx
+++ app/[from]/[to]/[day]/error.tsx
@@ -2,8 +2,12 @@
 
 export const Error = () => {
   return (
-    <div>
-      SOMETHING WENT WRONG
+    <div className="w-full flex flex-row justify-center pt-8">
+      <div className="w-full max-w-lg px-4 py-4 text-center bg-slate-50 dark:bg-slate-800  rounded-lg">
+        <p className="text-sm text-slate-500 dark:text-slate-200">
+          Kunde inte hämta resor
+        </p>
+      </div>
     </div>
   )
 }
