debb0b461384f47642dfb3e4f43d563fd857446c
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2023-04-17 11:34:04 +0200

    refactor: remove unused util function

--- utils/getBaseUrl.ts
+++
@@ -1,7 +0,0 @@
-import { cache } from 'react';
-
-export const getBaseUrl = cache(() =>
-  process.env.VERCEL_URL
-    ? `https://stockholm-uppsala.vercel.app`
-    : `http://localhost:${process.env.PORT ?? 3000}`,
-);
