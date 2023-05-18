dcb3273d926d7774d0ae144520d349b461f13c58
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2023-05-18 20:50:44 +0200

    style: update not found page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,32 @@
-import { LinkButton } from "@/components/LinkButton"
+import Link from "next/link";
+import { ChevronRight } from "@/assets/icons/ChevronRight";
+import { stationStockholm, stationUppsala } from "@/constants/stations";
+import dayjs from "dayjs";
 
 export const NotFound = () => (
   <div className="flex flex-col justify-center items-center mt-28 mb-20">
+    <div className="flex flex-col justify-center items-center p-8">
     <h2 className="text-base font-semibold text-indigo-600">404</h2>
 
-    <div className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-      Kan inte hitta sidan du letar efter
+    <div className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
+      Kan inte hitta sidan
     </div>
 
-    <p className="mt-4 text-base leading-7 text-gray-600">
+    <p className="mt-4 text-base leading-7 text-gray-600 dark:text-gray-400">
       Kan inte hitta sidan du letar efter.
     </p>
 
-    <div className="mt-10">
-      <LinkButton href="/">Hem</LinkButton>
+    <div className="mt-10 flex flex-row gap-2">
+      <Link href={`/`} className="relative bg-slate-700 hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 text-sm text-white h-12 min-h-[48px] px-8 rounded-lg flex items-center pointer-events-auto">
+        Hem
+      </Link>
+      <Link href={`/${stationStockholm}/${stationUppsala}/${dayjs().format("YYYY-MM-DD")}`} className="relative hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 text-sm text-slate-600 dark:text-white hover:text-white h-12 min-h-[48px] pl-8 pr-4 rounded-lg flex items-center pointer-events-auto">
+        Dagens tidtabell
+        <div className="ml-1">
+          <ChevronRight width="20" height="20" />
+        </div>
+      </Link>
+    </div>
     </div>
   </div>
 )
