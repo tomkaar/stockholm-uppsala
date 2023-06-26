5a76a43e5eb0e27cf33995c1d65fc721f47cf1f0
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2023-06-26 21:15:44 +0200

    feat: add back button to Header

--- app/[from]/[to]/[day]/components/Header.tsx
+++ app/[from]/[to]/[day]/components/Header.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link"
+
+import { Chevron } from "@/assets/icons/Chevron"
+
 import { FAQ } from "./FAQ"
 
 export interface IHeader {
@@ -8,7 +12,11 @@ export interface IHeader {
 export function Header({ from, to }: IHeader) {
   return (
     <header className="sticky top-0 z-20 flex flex-row justify-center items-center py-4 px-4 bg-white dark:bg-slate-900 border-b border-slate-900/10 dark:border-slate-700">
-      <h1 className="text-slate-900 dark:text-white text-xl tracking-light text-center font-bold">
+      <Link href="/" className="-rotate-90 text-slate-900">
+        <Chevron width={20} height={20} />
+      </Link>
+
+      <h1 className="text-slate-900 dark:text-white text-xl tracking-light text-center font-bold grow">
         {from}
         &nbsp;&nbsp;
         <span className="inline-block scale-x-[-1]">🚂</span>
